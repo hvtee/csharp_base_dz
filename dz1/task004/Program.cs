3fa2869d406432b9f1d616df2b01c6611beb4301
Artem <milkartem2@mail.ru>
2022-08-19 12:37:08 +0300

    Completed and added task004

--- dz1/task004/Program.cs
+++ dz1/task004/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace task001
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            System.Console.WriteLine("Input number.");
+            System.Console.Write("Number: ");
+            int number = Convert.ToInt32(Console.ReadLine()!);
+
+            int stepNumber = 1;
+
+            while (stepNumber <= number)
+            {
+                if (stepNumber % 2 != 1)
+            {
+                System.Console.Write(stepNumber + " ");
+            }
+            stepNumber++;
+            }
+            
+        }
+    }
+}
