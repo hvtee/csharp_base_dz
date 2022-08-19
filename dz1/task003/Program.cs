58f27040574e1a3b3c81efb0af7a3392eb41481e
Artem <milkartem2@mail.ru>
2022-08-19 12:19:21 +0300

    Completed and added task003

--- dz1/task003/Program.cs
+++ dz1/task003/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace task001
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            System.Console.WriteLine("Input number.");
+            System.Console.Write("Number: ");
+            int number = Convert.ToInt32(Console.ReadLine()!);
+            
+           if (Math.Abs(number) % 2 != 1)
+           {
+            System.Console.WriteLine($"Number {number} is even.");
+           }
+           else
+           {
+            System.Console.WriteLine($"Number {number} is NOT even.");
+           }
+        }
+    }
+}
