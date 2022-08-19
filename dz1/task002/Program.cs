ff03fe44b7867f635150f0afa8793a7bbfb81c35
Artem <milkartem2@mail.ru>
2022-08-19 12:15:27 +0300

    Completed and added task002

--- dz1/task002/Program.cs
+++ dz1/task002/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace task001
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            System.Console.WriteLine("Input two numbers.");
+            System.Console.Write("Number 1: ");
+            int numberA = Convert.ToInt32(Console.ReadLine()!);
+            System.Console.Write("Number 2: ");
+            int numberB = int.Parse((Console.ReadLine()!));
+            System.Console.Write("Number 3: ");
+            int numberC = int.Parse((Console.ReadLine()!));
+
+            int max = numberA;
+
+            if (numberB > max)
+            {
+                max = numberB;
+            }
+            if (numberC > max)
+            {
+                max = numberC;
+            }
+
+            System.Console.WriteLine($"Max is: {max}");
+        }
+    }
+}
