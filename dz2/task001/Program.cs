f7b7a4ef507d271b4cfdeb55391a40dd094450f4
Artem <milkartem2@mail.ru>
2022-08-24 09:15:10 +0300

    Added task001, work with pos and neg numbers

---
+++ dz2/task001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task001
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            System.Console.WriteLine("Input 3 digits number.");
+            System.Console.Write("Number: ");
+            int number = Convert.ToInt32(Console.ReadLine()!);
+
+            System.Console.WriteLine("Second digit is: " + Math.Abs((number % 100 - number % 10) / 10));
+        }
+    }
+}
