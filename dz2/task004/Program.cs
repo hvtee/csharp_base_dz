dce50bc91184cbbd5f599ffd0f918d7415098a59
Artem <milkartem2@mail.ru>
2022-08-24 09:34:49 +0300

    Added task004

---
+++ dz2/task004/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace task001
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            System.Console.WriteLine("Input three 3 digits number.");
+            System.Console.Write("Number1: ");
+            int number1 = Convert.ToInt32(Console.ReadLine()!);
+            System.Console.Write("Number2: ");
+            int number2 = Convert.ToInt32(Console.ReadLine()!);
+            System.Console.Write("Number3: ");
+            int number3 = Convert.ToInt32(Console.ReadLine()!);
+
+            int res = (number1 + number2 + number3) / 3;
+
+            if (res % 10 > (Math.Min(Math.Min(number1, number2), number3) % 10))
+            {
+                System.Console.WriteLine("Yes");
+            }
+            else if (res % 10 < (Math.Min(Math.Min(number1, number2), number3) % 10))
+            {
+                System.Console.WriteLine("No");
+            }
+            else
+            {
+                System.Console.WriteLine("Equal");
+            }
+        }
+    }
+}
