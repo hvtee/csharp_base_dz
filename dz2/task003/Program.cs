4297539bb209356883a6563a4a6bb341b827629c
Artem <milkartem2@mail.ru>
2022-08-24 09:24:13 +0300

    Added task003

---
+++ dz2/task003/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task001
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            System.Console.WriteLine("Input day number.");
+            System.Console.Write("Number: ");
+            int dayNumber = Convert.ToInt32(Console.ReadLine()!);
+
+            //System.Console.WriteLine(dayNumber==6||dayNumber==7? "Its day off":"Its NOT day off");
+
+            if (dayNumber == 6 || dayNumber == 7)
+            {
+                System.Console.WriteLine("Its day off");
+            }
+            else if (dayNumber > 0 && dayNumber < 6)
+            {
+                System.Console.WriteLine("Its NOT day off");
+            }
+            else
+            {
+                System.Console.WriteLine("Wrong day number.");
+            }
+        }
+    }
+}
