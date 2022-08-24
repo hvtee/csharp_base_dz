133350d96f0e5f20f554c2ea200b81688b8ed808
Artem <milkartem2@mail.ru>
2022-08-24 09:48:13 +0300

    Added task002

---
+++ dz2/task002/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task001
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            System.Console.WriteLine("Input number.");
+            System.Console.Write("Number: ");
+            int number = Convert.ToInt32(Console.ReadLine()!);
+
+            
+            Console.WriteLine(number > 99 ? number.ToString()[2] : "No third digit");
+            Console.ReadKey(true);
+        }
+    }
+}
