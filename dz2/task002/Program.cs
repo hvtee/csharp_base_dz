5f9e9dc3404da6b76d129ad6f3091dab718797d1
Artem <milkartem2@mail.ru>
2022-08-25 20:15:38 +0300

    Upgraded task002

--- dz2/task002/Program.cs
+++ dz2/task002/Program.cs
@@ -11,10 +11,23 @@ namespace task001
             System.Console.WriteLine("Input number.");
             System.Console.Write("Number: ");
             int number = Convert.ToInt32(Console.ReadLine()!);
+            number = Math.Abs(number);
 
-            
-            Console.WriteLine(number > 99 ? number.ToString()[2] : "No third digit");
-            Console.ReadKey(true);
+            //Console.WriteLine(number > 99 ? number.ToString()[2] : "No third digit");
+            //Console.ReadKey(true);
+
+            if (number < 99)
+            {
+                System.Console.WriteLine("No third digit");
+                return;
+            }
+
+            while (number > 999)
+            {
+                number = number / 10;
+            }
+
+            System.Console.WriteLine("Third digit is: " + number % 10);
         }
     }
 }
