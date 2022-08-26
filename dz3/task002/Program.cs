fcd3e291814e3b42dbaa2651f8f4496d2e83294f
Artem <milkartem2@mail.ru>
2022-08-26 17:44:59 +0300

    Added task002

---
+++ dz3/task002/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task001
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            System.Console.WriteLine("Input A(x, y, z)");
+            System.Console.Write("x: ");
+            int x1 = int.Parse(Console.ReadLine()!);
+            System.Console.Write("y: ");
+            int y1 = int.Parse(Console.ReadLine()!);
+            System.Console.Write("z: ");
+            int z1 = int.Parse(Console.ReadLine()!);
+            System.Console.WriteLine("Input B(x, y, z)");
+            System.Console.Write("x: ");
+            int x2 = Convert.ToInt16(Console.ReadLine());
+            System.Console.Write("y: ");
+            int y2 = Convert.ToInt16(Console.ReadLine());
+            System.Console.Write("z: ");
+            int z2 = int.Parse(Console.ReadLine()!);
+
+           
+            System.Console.WriteLine(Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2)+ Math.Pow(z2-z1, 2)));
+        }
+    }
+}
