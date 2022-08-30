437a6e4c9f51808e9ecc1454c3c5cc08c78e7f2e
Artem <milkartem2@mail.ru>
2022-08-30 19:13:49 +0300

    Added and completed task002

---
+++ dz4/task002/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace task002
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            int SumDigits(string num)
+            {
+                int res = (Convert.ToInt32(num[0]) - '0');
+                for (int i = 1; i < num.Length; i++)
+                {
+                    res += (Convert.ToInt32(num[i]) - '0');
+                }
+                return res;
+            }
+            System.Console.Write("Input number: ");
+            int number = Math.Abs(int.Parse(Console.ReadLine()!));
+            string num = Convert.ToString(number);
+
+
+            System.Console.WriteLine("The sum of digits is: " + SumDigits(num));
+
+        }
+    }
+}
