520aba90ae20b54f4c9ec24f7e7f4198073e241f
Artem <milkartem2@mail.ru>
2022-08-30 19:13:29 +0300

    Added task001

---
+++ dz4/task001/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task001
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            int ToPower(int numb, int pow)
+            {
+                int res = numb;
+                for (int i = 1; i < pow; i++)
+                {
+                    res *= numb;
+                }
+                return res;
+            }
+
+            System.Console.WriteLine("Input number and power.");
+            System.Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine()!);
+            System.Console.Write("Power: ");
+            int power = int.Parse(Console.ReadLine()!);
+
+            System.Console.WriteLine($"Number {number} power {power} is: " + ToPower(number, power));
+        }
+    }
+}
