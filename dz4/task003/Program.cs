0f1eec015a385985b374b0d7f2a89c89abf6b3e4
Artem <milkartem2@mail.ru>
2022-08-30 19:14:11 +0300

    Added and completed task003

---
+++ dz4/task003/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace task003
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            int[] FillArray(int arrayLength)
+            {
+                int[] list = new int[arrayLength];
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    list[i] = new Random().Next(-100, 101);
+                }
+                return list;
+            }
+            void PrintArray(int[] arr)
+            {
+                System.Console.Write("[ ");
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    System.Console.Write(arr[i] + ", ");
+                }
+                System.Console.Write("]");
+            }
+
+
+
+            System.Console.Write("Input array length: ");
+            int arrayLength = int.Parse(Console.ReadLine()!);
+            int[] array = FillArray(arrayLength);
+            System.Console.Write("The array is: ");
+            PrintArray(array);
+        }
+    }
+}
