96000ce76f59680762d2f49090be38e65642a9f9
Artem <milkartem2@mail.ru>
2022-09-06 21:01:40 +0300

    Added dz6

---
+++ dz6/task001_HowMany/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace task001
+{
+    class Program
+    {
+        static void Main()
+        {
+            void PrintArray(int[] arr)
+            {
+                foreach (var elem in arr)
+                {
+                    Console.Write(elem + " ");
+                }
+                Console.WriteLine(" ");
+            }
+            int[] CreateArray(int quantity)
+            {
+                int[] array = new int[quantity];              
+                for (int i = 0; i < quantity; i++)
+                {
+                    Console.Write($"arr[{i}] is: ");
+                    array[i] = int.Parse(Console.ReadLine()!);
+                }
+                return array;
+            }
+            int FindPositiveInArray(int[] arr)
+            {
+                int res = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] > 0) res += 1;    
+                }
+                return res;
+            }
+            /*int[] CreatePositiveArray(int[] arr)
+            {
+                int[] arrayPositive = new int[FindPositiveInArray(arr)];
+                for (int i = 0, j = 0; i < arr.Length; i++)
+                {
+                    if (arr[i]>0)
+                    {
+                        arrayPositive[j]=arr[i];
+                        j++;
+                    }
+                }
+                return arrayPositive;
+            }*/
+
+            Console.Clear();
+
+            Console.WriteLine("How many nums do you want to input: ");
+            int numQuantity = int.Parse(Console.ReadLine()!);
+
+            int[] array = CreateArray(numQuantity);
+            PrintArray(array);
+
+            Console.WriteLine("Number of pos in array is: " + FindPositiveInArray(array));
+
+            /*int[] arrPositive=CreatePositiveArray(array);
+            PrintArray(arrPositive);*/
+
+        }
+    }
+}
---
+++ dz6/task002_TwoFunc/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace task002
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            double FindX(double k1, double b1, double k2, double b2) => -(b1 - b2) / (k1 - k2); //ONLY X
+
+            double FindY(double k1, double b1, double k2, double b2) => (b1 * -k2 / k1 + b2) / (-k2 / k1 + 1); // ONLY Y
+            
+            string FindPoint(double k1, double b1, double k2, double b2)
+            {
+                string result="(";
+                result += Convert.ToString(-(b1 - b2) / (k1 - k2));
+                result += ", ";
+                result += Convert.ToString(k1 * (-(b1 - b2) / (k1 - k2)) + b1);
+                result += ")";
+                return result;
+            } // BOTH X ADND Y 
+
+            Console.Clear();
+
+            Console.WriteLine("y = k1 * x + b1 and y = k2 * x + b2");
+            Console.WriteLine("Input k1, b1, k2, b2");
+            Console.Write("k1: ");
+            double k1= double.Parse(Console.ReadLine()!);
+            Console.Write("b1: ");
+            double b1 = double.Parse(Console.ReadLine()!);
+            Console.Write("k2: ");
+            double k2 = double.Parse(Console.ReadLine()!);
+            Console.Write("b2: ");
+            double b2 = double.Parse(Console.ReadLine()!);
+
+            Console.WriteLine($"y = {k1} * x + {b1} ");
+            Console.WriteLine($"y = {k2} * x + {b2}");
+
+            Console.WriteLine($"Solution is: ({FindX(k1, b1, k2, b2)}, {FindY(k1, b1, k2, b2)})");
+            Console.WriteLine("Solution is: " + FindPoint(k1, b1, k2, b2));
+
+
+        }
+    }
+}
