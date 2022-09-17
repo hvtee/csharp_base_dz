3df6f2c3f1b78b810f2308e57148663055f2d32c
Artem <milkartem2@mail.ru>
2022-09-17 21:53:32 +0300

    Added dz9

---
+++ dz9/task001/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace task001
+{
+    class Program
+    {
+            static void ToEnum(int num, int lim)
+            {
+                if (num != lim)
+                {
+
+                    if (num%2==0 )Console.Write(num + " ");
+                    ToEnum(num + 1, lim);
+                }
+            }
+            static void Main()
+            {
+                Console.Clear();
+
+                int start = new Random().Next(-6, 6);
+                int end = new Random().Next(6, 16);
+
+                ToEnum(start, end);
+            }
+        }
+    }
---
+++ dz9/task002/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace task002
+{
+    class Program
+    {
+        static int SumDigits(ref int sum, int number, int lim)
+        {
+            if (number != lim)
+            {
+                if (number > 0) sum += number;
+                SumDigits(ref sum, number + 1, lim);
+                
+            }
+            return sum+lim;
+        }
+        static void Main()
+        {
+            Console.Clear();
+
+            int num = -3;
+            int lim = 7;
+            int sum = 0;
+            sum = SumDigits(ref sum, num, lim);
+            Console.WriteLine(sum);
+        }
+    }
+}
